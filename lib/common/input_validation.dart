38a01bf2f2ff831a6a72663bf593dc0738e69ac5
zeesooho <wltngh00@gmail.com>
2024-01-05 13:30:38 +0900

    add validation extension

---
+++ lib/common/input_validation.dart
@@ -0,0 +1,15 @@
+extension InputValidate on String {
+  //이메일 포맷 검증
+  bool isValidEmailFormat() {
+    return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(this);
+  }
+
+  //대쉬를 포함하는 010 휴대폰 번호 포맷 검증 (010-1234-5678)
+  bool isValidPhoneNumberFormat() {
+    return RegExp(r'^010-?([0-9]{4})-?([0-9]{4})$').hasMatch(this);
+  }
+
+  bool isValidPwFormat() {
+    return length >= 8 && contains(RegExp("[a-zA-Z]")) && contains(RegExp("[0-9]"));
+  }
+}
