0d02b762fcfadf8fbcd4cef230294345beb600dc
zeesooho <wltngh00@gmail.com>
2024-01-07 15:36:47 +0900

    add home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("home"),
+      ),
+      body: const Center(
+        child: Text("로그인 성공!"),
+      ),
+    );
+  }
+}
