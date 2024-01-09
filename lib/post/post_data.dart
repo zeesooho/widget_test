bbcb16fe1b63afa3bb93e250caec1c16766876b3
zeesooho <wltngh00@gmail.com>
2024-01-10 08:22:16 +0900

    add aditional info

--- lib/post/post_data.dart
+++ lib/post/post_data.dart
@@ -36,18 +36,21 @@ class PostUserData {
   final int id;
   final String name;
   final String? image;
+  final String? additionalInfo;
 
   PostUserData({
     required this.type,
     required this.id,
     required this.name,
     this.image,
+    this.additionalInfo,
   });
 
   factory PostUserData.fromJson(Map<String, dynamic> json) => PostUserData(
         type: json['type'],
         id: json['id'],
         name: json['name'],
-        image: json['image'].toString().isNotEmpty ? json['image'] : null,
+        image: json['image'],
+        additionalInfo: json['additionalInfo'],
       );
 }
