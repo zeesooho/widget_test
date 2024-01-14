65a633a0b3ad4f4bcf0afc335a2fcb07be343a06
zeesooho <wltngh00@gmail.com>
2024-01-14 18:50:34 +0900

    add widget_test

---
+++ lib/widget_test.dart
@@ -0,0 +1,14 @@
+export 'package:widget_test/login/login_widget.dart';
+
+export 'package:widget_test/signup/signup_widget.dart';
+export 'package:widget_test/signup/singup_dat.dart';
+
+export 'package:widget_test/profile/profile_card.dart';
+export 'package:widget_test/profile/profile_image.dart';
+
+export 'package:widget_test/post/create/create_post_widget.dart';
+
+export 'package:widget_test/post/list/post_card.dart';
+export 'package:widget_test/post/list/post_list.dart';
+
+export 'package:widget_test/post/detail/post_detail.dart';
