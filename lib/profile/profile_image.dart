a121b68c3f37b95a28563138602512c34756f9c2
zeesooho <wltngh00@gmail.com>
2024-01-30 17:05:58 +0900

    remove pad

--- lib/profile/profile_image.dart
+++ lib/profile/profile_image.dart
@@ -14,17 +14,14 @@ class ProfileImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 14.0),
-      child: uri != null
-          ? CircleAvatar(
-              radius: radius,
-              backgroundImage: NetworkImage(uri!),
-            )
-          : CircleAvatar(
-              radius: radius,
-              backgroundImage: defaultImage,
-            ),
-    );
+    return uri != null
+        ? CircleAvatar(
+            radius: radius,
+            backgroundImage: NetworkImage(uri!),
+          )
+        : CircleAvatar(
+            radius: radius,
+            backgroundImage: defaultImage,
+          );
   }
 }
