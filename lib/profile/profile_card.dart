0bf192a10ea2971af080117676876a0766c1bc86
zeesooho <wltngh00@gmail.com>
2024-02-27 17:10:25 +0900

    add profile card

--- lib/profile/profile_card.dart
+++ lib/profile/profile_card.dart
@@ -1,21 +1,62 @@
-import 'dart:math';
-
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:widget_test/profile/profile_data.dart';
+import 'package:widget_test/profile/profile_image.dart';
+
 class ProfileCard extends StatelessWidget {
-  const ProfileCard({super.key});
+  final VoidCallback? onTap;
+  final ProfileData _profileData;
+
+  const ProfileCard({
+    super.key,
+    this.onTap,
+    required ProfileData profileData,
+  }) : _profileData = profileData;
 
   @override
   Widget build(BuildContext context) {
-    var random = Random.secure();
-    return Container(
-      color: Color.fromARGB(
-        0xFF,
-        random.nextInt(255),
-        random.nextInt(255),
-        random.nextInt(255),
+    return Material(
+      borderRadius: BorderRadius.circular(20),
+      color: Colors.white,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(20),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: SizedBox(
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  child: Row(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 12),
+                        child: ProfileImage(radius: 30, defaultImage: AssetImage("asset/images/default_profile_image.jpg")),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(_profileData.job),
+                          Text(_profileData.company),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                    child: Text(
+                  _profileData.information,
+                  textAlign: TextAlign.start,
+                )),
+              ],
+            ),
+          ),
+        ),
       ),
-      height: 60,
     );
   }
 }
---
+++ lib/profile/profile_data.dart
@@ -0,0 +1,11 @@
+class ProfileData {
+  final String job;
+  final String company;
+  final String information;
+
+  ProfileData({
+    required this.job,
+    required this.company,
+    required this.information,
+  });
+}
